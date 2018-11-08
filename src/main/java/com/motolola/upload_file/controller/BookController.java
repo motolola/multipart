8747bdd081680c77270b41128dc516a322e6220e
Akin Motolola <akin.motolola@cognitran.com>
2018-11-08 16:35:39 +0000

    Added Dao class

--- src/main/java/com/motolola/upload_file/controller/BookController.java
+++ src/main/java/com/motolola/upload_file/controller/BookController.java
@@ -1,6 +1,18 @@
 package com.motolola.upload_file.controller;
 
+import com.motolola.upload_file.dao.BookDao;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+    private BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
 
 
 }
---
+++ src/main/java/com/motolola/upload_file/dao/BookDao.java
@@ -0,0 +1,8 @@
+package com.motolola.upload_file.dao;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDao {
+}
